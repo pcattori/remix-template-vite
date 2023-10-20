157e5cb59d62d95e32ac2a6d477cb4d007ed03cf
Brooks Lybrand <brookslybrand@gmail.com>
2023-10-19 20:38:10 -0500

    Add vite-tsconfig-paths and combine env types

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   optimizeDeps: {
     include: ["react", "react-dom/client"],
   },
-  plugins: [remix()],
+  plugins: [remix(), tsconfigPaths()],
 });
