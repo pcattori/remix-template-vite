c70c2fc6267d98a8d976249904a768214fe35a1d
Pedro Cattori <pcattori@gmail.com>
2023-10-22 00:43:16 -0400

    remove redundant `optimizeDeps` config
    Remix Vite plugin now sets those as a default for you

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  optimizeDeps: {
-    include: ["react", "react-dom/client"],
-  },
   plugins: [remix(), tsconfigPaths()],
 });
